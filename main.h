bf2f5f79b46c64d1d7bb2cf4f636383dba07fa87
njoroge-s <sarahnjoroge2015@gmail.com>
2022-07-12 03:05:02 -0700

    parameters fix

--- main.h
+++ main.h
@@ -73,4 +73,7 @@ int print_address(va_list l, flags_t *f);
 /* print_percent */
 int print_percent(va_list l, flags_t *f);
 
+/**parameters */
+void init_params(params_t *params, va_list ap);
+
 #endif
